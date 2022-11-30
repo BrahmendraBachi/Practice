0895125e9cc14c729a052053f74d37a71369a8c7
Brahmendra Bachi <hari8185008552@gamil.com>
2022-11-30 22:18:51 +0530

    Initial commit

---
+++ src/main/java/Trees/BTree.java
@@ -0,0 +1,43 @@
+package Trees;
+
+public class BTree {
+
+    public int data;
+
+    public BTree left;
+
+    public BTree right;
+
+    public BTree()
+    {
+        super();
+    }
+
+    public void addChild(int data, BTree root) {
+
+        if(data < root.data)
+        {
+            if(root.left != null)
+            {
+                addChild(data, root.left);
+            }
+            else{
+                BTree node = new BTree();
+                node.data = data;
+                root.left = node;
+            }
+        }
+        if(data > root.data)
+        {
+            if(root.right != null)
+            {
+                addChild(data, root.right);
+            }
+            else{
+                BTree node = new BTree();
+                node.data = data;
+                root.right = node;
+            }
+        }
+    }
+}
